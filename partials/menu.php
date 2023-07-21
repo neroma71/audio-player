60227dda5a0e0ec4c462cae353d204472afbddf4
maeldcls <maelduclos03@gmail.com>
2023-07-21 14:06:13 +0200

    almost finishedproject

--- partials/menu.php
+++ partials/menu.php
@@ -1,6 +1,6 @@
 <nav class="menu">
     <ul>
-        <li><a href="../index.php""><p class="title">SpotiWish</p></a></li>
+        <li><a href="../index.php"><p class="title">SpotiWish</p></a></li>
         <li><a href="../index.php">accueil</a></li>
         <li><a href="partials/login.php">connexion</a></li>
     </ul>
